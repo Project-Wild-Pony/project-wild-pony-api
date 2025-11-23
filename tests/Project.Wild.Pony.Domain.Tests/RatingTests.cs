d6677dd225da5fdf10650d7fb8fac02dfddfaafb
XianzhenHe <m18518223268@gmail.com>
2025-11-23 12:26:19 -0500

    Lab 5 - Part 1: API tests and endpoints
     Changes to be committed:
    	modified:   Project.Wild.Pony.Api/api-tests.http
    	modified:   Project.Wild.Pony.sln
    	new file:   tests/Project.Wild.Pony.Domain.Tests/ItemTests.cs
    	new file:   tests/Project.Wild.Pony.Domain.Tests/MSTestSettings.cs
    	new file:   tests/Project.Wild.Pony.Domain.Tests/Project.Wild.Pony.Domain.Tests.csproj
    	new file:   tests/Project.Wild.Pony.Domain.Tests/RatingTests.cs

---
+++ tests/Project.Wild.Pony.Domain.Tests/RatingTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Project.Wild.Pony.Domain.Catalog;   
+
+namespace Project.Wild.Pony.Domain.Tests
+{
+    [TestClass]
+    public sealed class RatingTests
+    {
+        
+        [TestMethod]
+        public void Can_Create_New_Rating()
+        {
+           
+            var rating = new Rating(1, "Mike", "Great fit!");
+
+           
+            Assert.AreEqual(1, rating.Stars);
+            Assert.AreEqual("Mike", rating.UserName);
+            Assert.AreEqual("Great fit!", rating.Review);
+        }
+
+      
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Cannot_Create_Rating_With_Invalid_Stars()
+        {
+           
+            var rating = new Rating(0, "Mike", "Great fit!");
+        }
+    }
+}
